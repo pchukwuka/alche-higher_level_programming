4b6953060553561628d36096642f4756be02e7d8
pchukwuka <p.chukwuka@alustudent.com>
2025-09-01 05:26:12 -0700

    class square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initialize the square with a private size"""
+        self.__size = size
